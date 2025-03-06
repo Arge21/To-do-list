d2b125d16e7324aebde52106a35df0452970fcf4
Arge_21 <115039825+Arge21@users.noreply.github.com>
2025-03-06 15:11:41 -0500

    (fin) finish project

--- script.js
+++ script.js
@@ -1,5 +1,36 @@
-let btn = document.querySelector("#taskButton");
+let btn = document.querySelector("#taskBtn");
 let taskInput = document.querySelector("#taskInput");
 let list = document.querySelector("ul");
 
 
+function run() {
+    textInput = taskInput.value;
+    taskInput.value = ' ';
+    
+    let listItem = document.createElement("li");
+    let spanElement = document.createElement("span");
+    let delBtn = document.createElement("button");
+
+
+    delBtn.style.marginLeft = '10px'
+    listItem.appendChild(spanElement);
+    listItem.appendChild(delBtn);
+
+    spanElement.textContent = textInput;
+    delBtn.textContent = 'Delete';
+    list.appendChild(listItem);
+
+    listItem.style.cursor = 'pointer'
+    listItem.addEventListener ("click", () => {
+        listItem.style.cssText = 'text-decoration-color: green; text-decoration-line: line-through;'
+    });
+
+    delBtn.addEventListener ("click", () => {
+        list.removeChild(listItem)
+    });
+
+  
+    focus()
+};
+
+btn.addEventListener('click', run);
